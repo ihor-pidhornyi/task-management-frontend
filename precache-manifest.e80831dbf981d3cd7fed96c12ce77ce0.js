57086e057b78ab4a3c1871164bc0dd52022d1567
ihor-pidhornyi <ihor-pidhornyi@users.noreply.github.com>
2021-11-07 09:19:10 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.e80831dbf981d3cd7fed96c12ce77ce0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c83b4df6a6898ccd1b89413fcc5fa06",
+    "url": "https://ihor-pidhornyi.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1be489a104f491bc4ed1",
+    "url": "https://ihor-pidhornyi.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0b7f828dec0083eff5d0",
+    "url": "https://ihor-pidhornyi.github.io/task-management-frontend/static/js/2.7875803d.chunk.js"
+  },
+  {
+    "revision": "1be489a104f491bc4ed1",
+    "url": "https://ihor-pidhornyi.github.io/task-management-frontend/static/js/main.12cc8710.chunk.js"
+  },
+  {
+    "revision": "81f1bdc94904eacb6784",
+    "url": "https://ihor-pidhornyi.github.io/task-management-frontend/static/js/runtime~main.6b60504c.js"
+  }
+]);
